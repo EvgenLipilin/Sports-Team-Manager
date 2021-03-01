5e989e42d30215b8afd00980ddb1bf823fd6f9ae
EvgenLipilin <evgenlipilin@yandex.ru>
2021-03-01 16:12:36 +0300

    Добаботка действия с кнопкой и констрейнсы

--- SportManager/SportManager/AppDelegate.swift
+++ SportManager/SportManager/AppDelegate.swift
@@ -9,19 +9,24 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     
-
-
+    let dataManager = CoreDataManager(modelName: "Player")
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-  
-        //let rootVC = MainViewController()
+        
+        let rootVC = MainViewController()
+        rootVC.dataManager = dataManager
+        
+        let navigationController = UINavigationController(rootViewController: rootVC)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         
         return true
     }
-
-
-
+    
+    
 }
 
--- SportManager/SportManager/View/PlayerCell.swift
+++ SportManager/SportManager/View/PlayerCell.swift
@@ -9,34 +9,36 @@ import UIKit
 
 class PlayerCell: UITableViewCell {
     
-    let identifierCell = "PlayerCell"
+    static let identifierCell = "PlayerCell"
     
     let number: UILabel = {
         let number = UILabel()
-        number.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        number.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        number.text = "7"
         number.textAlignment = .center
         number.translatesAutoresizingMaskIntoConstraints = false
         return number
     }()
     
     let avatar: UIImageView = {
-       let avatar = UIImageView()
+        let avatar = UIImageView()
         avatar.translatesAutoresizingMaskIntoConstraints = false
         avatar.contentMode = .scaleAspectFill
         avatar.backgroundColor = .systemGray
-        avatar.image = #imageLiteral(resourceName: "avatar")
+        avatar.image = #imageLiteral(resourceName: "man")
+        avatar.layer.cornerRadius =   avatar.frame.size.width / 2
         avatar.clipsToBounds = true
         return avatar
     }()
     
     let fullName: UILabel = {
-       let fullName = UILabel()
+        let fullName = UILabel()
         fullName.translatesAutoresizingMaskIntoConstraints = false
         return fullName
     }()
     
     let team: UILabel = {
-       let team = UILabel()
+        let team = UILabel()
         team.translatesAutoresizingMaskIntoConstraints = false
         team.text = "Team"
         team.font = .systemFont(ofSize: 14, weight: .bold)
@@ -46,11 +48,12 @@ class PlayerCell: UITableViewCell {
     let teamDescriptionLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Text"
         return label
     }()
     
     let nationality: UILabel = {
-       let nationality = UILabel()
+        let nationality = UILabel()
         nationality.translatesAutoresizingMaskIntoConstraints = false
         nationality.text = "Nationality"
         nationality.font = .systemFont(ofSize: 14, weight: .bold)
@@ -60,11 +63,12 @@ class PlayerCell: UITableViewCell {
     let nationalityDescriptionLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Text"
         return label
     }()
     
     let position: UILabel = {
-       let position = UILabel()
+        let position = UILabel()
         position.translatesAutoresizingMaskIntoConstraints = false
         position.text = "Position"
         position.font = .systemFont(ofSize: 14, weight: .bold)
@@ -78,7 +82,7 @@ class PlayerCell: UITableViewCell {
     }()
     
     let age: UILabel = {
-       let age = UILabel()
+        let age = UILabel()
         age.translatesAutoresizingMaskIntoConstraints = false
         age.text = "Age"
         age.font = .systemFont(ofSize: 14, weight: .bold)
@@ -88,13 +92,6 @@ class PlayerCell: UITableViewCell {
     let ageDescriptionLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let playerStatusLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
         return label
     }()
     
@@ -119,31 +116,83 @@ class PlayerCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super .init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func createCell(_model: Player){
-        number.text = "\(_model.number)"
-        avatar.image = UIImage(data: _model.image ?? Data())
-        fullName.text = _model.fullname
-        teamDescriptionLabel.text = _model.club?.name
-        positionDescriptionLabel.text = _model.position
-        ageDescriptionLabel.text = "\(_model.age)"
-        playerStatusLabel.text = _model.inPlay ? "In Play" : "Bench"
+    func createCell(model: Player){
+        number.text = "\(model.number)"
+        avatar.image = UIImage(data: model.image ?? Data())
+        fullName.text = model.fullName
+        teamDescriptionLabel.text = model.club?.name
+        positionDescriptionLabel.text = model.position
+        ageDescriptionLabel.text = "\(model.age)"
+        nationalityDescriptionLabel.text = model.nationality
         
     }
     
     func setupLayout() {
-        [number,fullName,playerStatusLabel,avatar,mainStack,leftStack,rightStack,team,nationality,position,age,teamDescriptionLabel,nationalityDescriptionLabel,positionDescriptionLabel,ageDescriptionLabel].forEach { (element) in
+        [number,fullName,avatar,mainStack,leftStack,rightStack,team,nationality,position,age,teamDescriptionLabel,nationalityDescriptionLabel,positionDescriptionLabel,ageDescriptionLabel].forEach { (element) in
             addSubview(element)
         }
         let inset: CGFloat = 10
         let imageSize: CGFloat = 125
         let halfMainStackViewWidth = (frame.width - (inset * 3)) / 2.5
         
-        NSLayoutConstraint.activate([number])
-    }
+        NSLayoutConstraint.activate(([number.topAnchor.constraint(equalTo: topAnchor, constant: inset),
+                                      number.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset),
+                                      number.widthAnchor.constraint(equalToConstant: 25),
+                                      number.heightAnchor.constraint(equalToConstant: 25),
+                                      
+                                      fullName.centerYAnchor.constraint(equalTo: number.centerYAnchor),
+                                      fullName.leadingAnchor.constraint(equalTo: number.trailingAnchor, constant: inset),
+                                      
+                                      avatar.topAnchor.constraint(equalTo: number.bottomAnchor, constant: inset),
+                                      avatar.leadingAnchor.constraint(equalTo: number.leadingAnchor),
+                                      avatar.heightAnchor.constraint(equalToConstant: imageSize),
+                                      avatar.widthAnchor.constraint(equalToConstant: imageSize),
+                                      
+                                      mainStack.topAnchor.constraint(equalTo: avatar.topAnchor),
+                                      mainStack.leadingAnchor.constraint(equalTo: avatar.trailingAnchor, constant: inset),
+                                      mainStack.bottomAnchor.constraint(equalTo: avatar.bottomAnchor),
+                                      mainStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset),
+                                      
+                                      leftStack.topAnchor.constraint(equalTo: mainStack.topAnchor),
+                                      leftStack.leadingAnchor.constraint(equalTo: mainStack.leadingAnchor),
+                                      leftStack.bottomAnchor.constraint(equalTo: mainStack.bottomAnchor),
+                                      leftStack.widthAnchor.constraint(equalToConstant: halfMainStackViewWidth),
+                                      
+                                      team.topAnchor.constraint(equalTo: leftStack.topAnchor, constant: inset),
+                                      team.leadingAnchor.constraint(equalTo: leftStack.leadingAnchor, constant: inset),
+                                      
+                                      nationality.topAnchor.constraint(equalTo: team.bottomAnchor, constant: inset),
+                                      nationality.leadingAnchor.constraint(equalTo: team.leadingAnchor),
+                                      
+                                      position.topAnchor.constraint(equalTo: nationality.bottomAnchor, constant: inset),
+                                      position.leadingAnchor.constraint(equalTo: nationality.leadingAnchor),
+                                      
+                                      age.topAnchor.constraint(equalTo: position.bottomAnchor, constant: inset),
+                                      age.leadingAnchor.constraint(equalTo: position.leadingAnchor),
+                                      
+                                      rightStack.topAnchor.constraint(equalTo: leftStack.topAnchor),
+                                      rightStack.leadingAnchor.constraint(equalTo: leftStack.trailingAnchor),
+                                      rightStack.bottomAnchor.constraint(equalTo: leftStack.bottomAnchor),
+                                      rightStack.trailingAnchor.constraint(equalTo: mainStack.trailingAnchor),
+                                      
+                                      teamDescriptionLabel.topAnchor.constraint(equalTo: team.topAnchor),
+                                      teamDescriptionLabel.leadingAnchor.constraint(equalTo: rightStack.leadingAnchor),
+                                      
+                                      nationalityDescriptionLabel.topAnchor.constraint(equalTo: nationality.topAnchor),
+                                      nationalityDescriptionLabel.leadingAnchor.constraint(equalTo: teamDescriptionLabel.leadingAnchor),
+                                      
+                                      positionDescriptionLabel.topAnchor.constraint(equalTo: position.topAnchor),
+                                      positionDescriptionLabel.leadingAnchor.constraint(equalTo: teamDescriptionLabel.leadingAnchor),
+                                      
+                                      ageDescriptionLabel.topAnchor.constraint(equalTo: age.topAnchor),
+                                      ageDescriptionLabel.leadingAnchor.constraint(equalTo: teamDescriptionLabel.leadingAnchor)]))
+}
 }
